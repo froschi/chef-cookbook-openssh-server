3518df5ddc84d24a353a0f23a16d31d85e408f49
Thorsten Fischer <thorsten@froschi.org>
2012-06-13 19:06:52 +0200

    Added cookbook dependencies.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,10 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 %w/ubuntu/.each { |os| supports os }
+
+depends          "libcomerr", ">= 0.0.1"
+depends          "libkrb5", ">= 0.0.1"
+depends          "libpam", ">= 0.0.1"
+depends          "libselinux", ">= 0.0.1"
+depends          "libwrap", ">= 0.0.1"
+depends          "openssh-client", ">= 0.0.1"
